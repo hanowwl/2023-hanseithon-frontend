6a6fe816497e0facd0ad30bfd00a59b348489260
HanSu Lee <i.hansu.lee@gmail.com>
2023-06-19 19:54:11 +0900

    feat(components): add `Navbar` component

---
+++ src/components/common/Navbar/styled.ts
@@ -0,0 +1,67 @@
+import Link from "next/link";
+
+import styled from "@emotion/styled";
+
+import { Container } from "src/components/layouts";
+
+export const NavbarContainer = styled.nav`
+  position: sticky;
+  top: 0;
+  width: 100%;
+  height: 9rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background: rgba(0, 0, 0, 0.3);
+  backdrop-filter: blur(8px);
+`;
+
+export const NavbarInnerContainer = styled(Container)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const NavbarBrandLink = styled(Link)`
+  text-align: center;
+  text-decoration: none;
+  user-select: none;
+  color: ${(props) => props.theme.color.text.primary.default};
+`;
+
+export const NavbarBrandText = styled.h1`
+  font-weight: 800;
+  font-size: 2.4rem;
+  letter-spacing: 0.04em;
+  margin-bottom: 0.3rem;
+`;
+
+export const NavbarBrandSubText = styled.h2`
+  font-weight: 400;
+  font-size: 1.2rem;
+  letter-spacing: -0.01em;
+`;
+
+export const NavbarMenuList = styled.ul`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 4rem;
+`;
+
+export const NavbarMenuItem = styled.li`
+  float: left;
+`;
+
+export const NavbarMenuLink = styled(Link)`
+  font-weight: 600;
+  text-align: center;
+  text-decoration: none;
+  color: ${(props) => props.theme.color.text.primary.default};
+`;
