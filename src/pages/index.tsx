acebf4c5476a11e659dbfac195fe40dc37e37261
alpha0212 <qkrcjfgml321@naver.com>
2023-06-21 23:41:32 +0900

    feat(components): add ThridSection component with props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,31 @@
+import { FirstSection, SecondSection, ThirdSection } from "src/components/main";
+
 export default function HomePage() {
-  return <div>Hello, World!</div>;
+  const ThirdDataArr = [
+    {
+      emoji: "😎",
+      title: "누적 참여 학생",
+      count: "230명 +",
+      desc: "",
+    },
+    {
+      emoji: "🌈",
+      title: "운영 기간",
+      count: "6회차",
+      desc: "2018년부터",
+    },
+    {
+      emoji: "😵",
+      title: "갈려나간 노예들",
+      count: "120명 +",
+      desc: "자랑스럽다! 무!급!노!예!",
+    },
+  ];
+  return (
+    <div>
+      <FirstSection operatingPeriod="JULY 20 - JULY 21" receiptPeriod="접수 기간 : 7. 10 ~ 7. 19" />
+      <SecondSection />
+      <ThirdSection ThirdArr={ThirdDataArr} />
+    </div>
+  );
 }
