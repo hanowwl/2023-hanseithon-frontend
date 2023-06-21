ae9a222235162d274d5f0c0d2f228029362f0fb6
alpha0212 <qkrcjfgml321@naver.com>
2023-06-21 14:21:19 +0900

    feat(components): add SecondSection component

---
+++ src/components/main/SecondSection/styled.ts
@@ -0,0 +1,50 @@
+import styled from "@emotion/styled";
+
+export const SecondSection = styled.section`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+export const WhatHackathonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const WhatHackathon = styled.strong`
+  color: #fff;
+
+  font-size: 4.4rem;
+  font-weight: 800;
+
+  margin: 18.2rem auto 0 auto;
+  line-height: 5.3rem;
+`;
+
+export const EmojiContainer = styled.div`
+  display: flex;
+
+  margin: 5.8rem auto;
+`;
+
+export const Emoji = styled.div`
+  color: #fff;
+  font-size: 9.6rem;
+  font-weight: 600;
+
+  line-height: 11.5rem;
+`;
+export const Sum = styled.div`
+  font-size: 6rem;
+  font-weight: 500;
+
+  text-align: center;
+  line-height: 7.2rem;
+
+  margin: auto 6.4rem auto 6.4rem;
+`;
+
+export const HackathonDescriptionContainer = styled.div`
+  margin: 11.8rem auto 0 auto;
+`;
