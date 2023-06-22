9c74b3bd9f145e8aab6d131988146d212218421f
alpha0212 <qkrcjfgml321@naver.com>
2023-06-22 15:54:32 +0900

    feat(styles): remove scrollbar design in globar style

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -9,7 +9,7 @@ export const globalStyle = (theme: Theme) => css`
   body,
   #__next {
     width: 100%;
-    height: 100%;
+    min-height: 100%;
   }
 
   html {
@@ -28,17 +28,5 @@ export const globalStyle = (theme: Theme) => css`
       Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic",
       "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
     letter-spacing: -0.04em;
-  }
-  ::-webkit-scrollbar {
-    width: 10px;
-  }
-
-  ::-webkit-scrollbar-thumb {
-    background: linear-gradient(180deg, #a3d2ff 0%, #64a6e2 99.99%); /* 스크롤바 색상 */
-    border-radius: 10px; /* 스크롤바 둥근 테두리 */
-  }
-
-  ::-webkit-scrollbar-track {
-    background: rgba(255, 255, 255); /*스크롤바 뒷 배경 색상*/
   }
 `;
