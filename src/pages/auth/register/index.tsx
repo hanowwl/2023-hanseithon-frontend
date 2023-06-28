a6dd8ea2ca98b989e161335861ee712ed7ad6225
HanSu Lee <i.hansu.lee@gmail.com>
2023-06-28 15:56:36 +0900

    feat(pages): add register mutation into `AuthRegisterPage`

--- src/pages/auth/register/index.tsx
+++ src/pages/auth/register/index.tsx
@@ -1,23 +1,41 @@
 import React, { useState } from "react";
+import { toast } from "react-toastify";
+
+import { useRouter } from "next/router";
 
 import { RegisterForms, RegisterForm, AuthForm } from "src/components/auth";
-import { useFunnel } from "src/hooks";
+import { useFunnel, useRegisterMutation } from "src/hooks";
 
 export default function AuthRegisterPage() {
   const [Funnel, setStep] = useFunnel(["step1", "step2", "step3"] as const);
-  const [formState, setFormState] = useState<RegisterForms | object>({});
+  const [formState, setFormState] = useState<Partial<RegisterForms>>({});
+
+  const router = useRouter();
+  const registerMutation = useRegisterMutation();
 
   const handleOnSubmit = () => {
-    console.log(formState);
+    if (!formState) return;
+    const { email, password, passwordVerify, name, ...student } = formState as RegisterForms;
+
+    registerMutation.mutate(
+      { email, password, name, student },
+      {
+        onSuccess: () => {
+          toast.success("회원가입이 완료되었어요");
+          router.push("/auth/login");
+        },
+        onError: ({ response }) => toast.error(response?.data.message),
+      }
+    );
   };
 
   return (
-    <AuthForm.Layout name="회원가입" description="">
+    <AuthForm.Layout name="회원가입" description="회원가입 후 한세톤에 참여해보세요 !">
       <Funnel>
         <Funnel.Step name="step1">
           <RegisterForm.Step1
             onNext={(values) => {
-              setFormState((prev) => ({ ...prev, ...values }));
+              setFormState({ ...values });
               setStep("step2");
             }}
           />
@@ -34,6 +52,7 @@ export default function AuthRegisterPage() {
 
         <Funnel.Step name="step3">
           <RegisterForm.Step3
+            loading={registerMutation.isLoading}
             onNext={(values) => {
               setFormState((prev) => ({ ...prev, ...values }));
               handleOnSubmit();
