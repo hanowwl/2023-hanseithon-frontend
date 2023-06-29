310ba8b6b842e64215a94c69e7bb87edfc3f051a
alpha0212 <qkrcjfgml321@naver.com>
2023-06-29 22:05:01 +0900

    feat(components): add SixthSection, SeventhSection component

--- src/components/main/index.ts
+++ src/components/main/index.ts
@@ -3,3 +3,5 @@ export * from "./SecondSection";
 export * from "./ThirdSection";
 export * from "./FourthSection";
 export * from "./FiveSection";
+export * from "./SixthSection";
+export * from "./SeventhSection";
