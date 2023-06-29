2e80144248865edb4cc4188274845b23fe279956
alpha0212 <qkrcjfgml321@naver.com>
2023-06-29 21:59:40 +0900

    feat(constants): added faq data

---
+++ src/constants/faq.ts
@@ -0,0 +1,22 @@
+import { SeventhFAQ } from "src/components/main";
+
+export const faq: SeventhFAQ[] = [
+  {
+    question: "Q. 한 팀에 최소/최대 몇명까지 참여할 수 있나요?",
+    answer:
+      "A. 한 팀은 포지션(개발/디자인/기획) 상관 없이 최소 3명에서 최대 5명까지 구성할 수 있습니다.",
+  },
+  {
+    question: "Q. 해커톤 진행 중 식사는 어떻게 제공되나요?",
+    answer:
+      "A. 진행 중 식사는 당연히 제공되며 야식도 같이 제공될 예정입니다.   식사 메뉴는 해커톤 진행 중 안내될 예정입니다.",
+  },
+  {
+    question: "Q. 외부 라이브러리 사용이 가능한가요?",
+    answer: "네 됩니다.",
+  },
+  {
+    question: "Q. Chat GPT와 Bard 같은 AI 사용이 가능한가요?",
+    answer: "되겠냐",
+  },
+];
