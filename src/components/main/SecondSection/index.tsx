8a5d688fb9a18761cb8895f084e3586c78c32b98
HanSu Lee <i.hansu.lee@gmail.com>
2023-07-02 00:44:57 +0900

    refactor(components): rename variables and modify some styles in `SecondSection` of main page

--- src/components/main/SecondSection/index.tsx
+++ src/components/main/SecondSection/index.tsx
@@ -5,27 +5,25 @@ import * as S from "./styled";
 const SecondSection: React.FC = () => {
   return (
     <S.SecondSection>
-      <S.WhatHackathonContainer>
-        <S.WhatHackathon>해커톤이 뭔가요?</S.WhatHackathon>
-        <S.EmojiContainer>
-          <S.Emoji>🤖</S.Emoji>
-          <S.Sum>+</S.Sum>
-          <S.Emoji>🏃🏻‍♂️</S.Emoji>
-        </S.EmojiContainer>
-      </S.WhatHackathonContainer>
-      <S.HackathonDescriptionContainer>
+      <S.SecondSectionInnerContainer>
+        <S.SecondSectionTitle>해커톤이 뭔가요?</S.SecondSectionTitle>
+
+        <S.EmojiText style={{ margin: "5.8rem 0 11.8rem 0" }}>
+          <S.Emoji>🧑‍💻</S.Emoji>+<S.Emoji>🏃</S.Emoji>
+        </S.EmojiText>
+
         <S.HackathonDescription>
           해커톤은 해킹과 마라톤의 합성어로
           <br />
           <br />
-          소프트웨어 개발 분야의 프로그래머, 디자이너, 기획자 등 <br />
-          다양한 분야에서 모여
-          <S.HackathonDescriptionPoint>
-            제한된 시간 안에 아이디어를 도출하고 결과물을 만들어 내는 이벤트
-          </S.HackathonDescriptionPoint>
-          에요
+          소프트웨어 개발 분야의 프로그래머, 디자이너, 기획자 등
+          <br />
+          <strong>
+            다양한 분야에서 모여 제한된 시간 안에 아이디어를 도출하고 결과물을 만들어 내는
+            이벤트에요
+          </strong>
         </S.HackathonDescription>
-      </S.HackathonDescriptionContainer>
+      </S.SecondSectionInnerContainer>
     </S.SecondSection>
   );
 };
