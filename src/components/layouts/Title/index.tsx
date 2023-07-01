af2584afba14e0e1ed336ed700faa91b3da2f9e7
alpha0212 <qkrcjfgml321@naver.com>
2023-07-01 14:30:19 +0900

    feat(components): add Title component

---
+++ src/components/layouts/Title/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import * as S from "./styled";
+
+export interface TitleProps {
+  mainText: string;
+  descText: string;
+}
+
+const Title: React.FC<TitleProps> = ({ mainText, descText }) => {
+  return (
+    <S.TitleContainer>
+      <S.TitleText>{mainText}</S.TitleText>
+      <S.TitleDesc>{descText}</S.TitleDesc>
+    </S.TitleContainer>
+  );
+};
+
+export { Title };
