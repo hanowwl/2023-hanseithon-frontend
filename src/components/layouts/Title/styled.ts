5a967172fb317d4b5c78b9b51ec4f6a9e700b975
alpha0212 <qkrcjfgml321@naver.com>
2023-07-01 14:30:19 +0900

    feat(components): add Title component

---
+++ src/components/layouts/Title/styled.ts
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin-top: 2rem;
+`;
+
+export const TitleText = styled.h1`
+  font-size: 2.4rem;
+  font-weight: 700;
+
+  margin-bottom: 0.6rem;
+`;
+
+export const TitleDesc = styled.span`
+  color: #878b92;
+  font-size: 1.6rem;
+  font-weight: 500;
+
+  letter-spacing: -0.064rem;
+`;
