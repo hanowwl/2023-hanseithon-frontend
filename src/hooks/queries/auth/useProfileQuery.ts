c9824d67f0f06728cb33cbc287d70b90772abbb6
HanSu Lee <i.hansu.lee@gmail.com>
2023-07-01 14:16:01 +0900

    feat(hooks): add `statelTime` and modify repsonse of `useProfileQuery` hook

--- src/hooks/queries/auth/useProfileQuery.ts
+++ src/hooks/queries/auth/useProfileQuery.ts
@@ -1,17 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { GetProfileResponse, api } from "src/apis";
-import { APIResponse } from "src/apis/core/types";
 
 import { keys } from "../keys";
 import { UseQueryCustomOptions } from "../types";
 
-export const useProfileQuery = (
-  options: UseQueryCustomOptions<APIResponse<GetProfileResponse>>
-) => {
+export const useProfileQuery = (options?: UseQueryCustomOptions<GetProfileResponse>) => {
   return useQuery({
     queryKey: keys.auth.profile,
-    queryFn: () => api.auth.getProfile(),
+    queryFn: () => api.auth.getProfile().then((res) => res.result),
+    staleTime: 1000 * 60 * 10,
     ...options,
   });
 };
