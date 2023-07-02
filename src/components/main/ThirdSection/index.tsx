0da6ef167af89af4851235d46009a885262632d3
HanSu Lee <i.hansu.lee@gmail.com>
2023-07-02 19:25:31 +0900

    style(components): remove comments in `ThirdSection` of main page

--- src/components/main/ThirdSection/index.tsx
+++ src/components/main/ThirdSection/index.tsx
@@ -4,20 +4,13 @@ import { EmojiCard, EmojiCardProps } from "src/components/common";
 
 import * as S from "./styled";
 
-// export interface RecordItem {
-//   emoji: string;
-//   title: string;
-//   value: string;
-//   description: string;
-// }
-
 export interface MainThirdSectionProps {
   records: EmojiCardProps[];
 }
 
 const ThirdSection: React.FC<MainThirdSectionProps> = ({ records }) => {
   return (
-    <>
+    <section>
       <S.ThirdInnerContainer>
         <S.ThirdSectionTitle>드디어 6년째을 맞이한, 한세톤</S.ThirdSectionTitle>
         <S.ThirdSectionDescription>이젠 없어선 안될 한세의 전통으로</S.ThirdSectionDescription>
@@ -28,28 +21,7 @@ const ThirdSection: React.FC<MainThirdSectionProps> = ({ records }) => {
           ))}
         </S.EmojiCardList>
       </S.ThirdInnerContainer>
-
-      {/* <S.ThirdContent>
-        <S.ThirdTitle>드디어 6년째을 맞이한, 한세톤</S.ThirdTitle>
-        <S.ThirdDescription>이젠 없어선 안될 한세의 전통으로</S.ThirdDescription>
-        <S.ContentBoxContainer>
-          {records.map((content, idx) => {
-            return (
-              <S.ContentBox key={idx}>
-                <S.ContainerInContentBox>
-                  <S.ContentEmojiBox>
-                    <S.ContentEmoji>{content.emoji}</S.ContentEmoji>
-                  </S.ContentEmojiBox>
-                  <S.ContentTitle>{content.title}</S.ContentTitle>
-                  <S.ContentCount>{content.value}</S.ContentCount>
-                  <S.ContentDesc>{content.description}</S.ContentDesc>
-                </S.ContainerInContentBox>
-              </S.ContentBox>
-            );
-          })}
-        </S.ContentBoxContainer>
-      </S.ThirdContent> */}
-    </>
+    </section>
   );
 };
 
