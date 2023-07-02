640e9d9318be8f92c595991c61b2aac86732647d
HanSu Lee <i.hansu.lee@gmail.com>
2023-07-03 05:59:47 +0900

    fix(components,constants): fix typos in main page sections

--- src/constants/navbar.ts
+++ src/constants/navbar.ts
@@ -1,7 +1,7 @@
 import { NavbarProps } from "src/components/common";
 
 export const NAVBAR_MENU: NavbarProps["menu"] = [
-  { text: "한세톤 소개", href: "/" },
-  { text: "대회 일정", href: "/schedules" },
-  { text: "후원사", href: "/sponsors" },
+  // { text: "한세톤 소개", href: "/" },
+  // { text: "대회 일정", href: "/schedules" },
+  // { text: "후원사", href: "/sponsors" },
 ];
