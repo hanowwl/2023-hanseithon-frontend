88469634707876c1264b7ec2b0c07e79d118491a
alpha0212 <qkrcjfgml321@naver.com>
2023-07-05 01:03:12 +0900

    feat(components): type assign and change elements styles by boolean

--- src/components/schedule/Element/index.tsx
+++ src/components/schedule/Element/index.tsx
@@ -2,21 +2,22 @@ import React from "react";
 
 import * as S from "./styled";
 
-export interface ScheduleElementProps {
-  time: string;
-  nowSchedule: string;
-  nowCurrent: boolean;
-}
-
 export interface ScheduleData {
   time: string;
   nowSchedule: string;
 }
 
-const Element: React.FC<ScheduleElementProps> = ({ time, nowSchedule, nowCurrent }) => {
+export interface ScheduleElementProps {
+  time: string;
+  nowSchedule: string;
+  nowCurrent: boolean;
+  dayBoolean: boolean;
+}
+
+const Element: React.FC<ScheduleElementProps> = ({ time, nowSchedule, nowCurrent, dayBoolean }) => {
   return (
     <S.ScheduleElement>
-      <S.ScheduleTime>{time}</S.ScheduleTime>
+      <S.ScheduleTime current={dayBoolean}>{time}</S.ScheduleTime>
       <S.ScheduleNow current={nowCurrent}>{nowSchedule}</S.ScheduleNow>
     </S.ScheduleElement>
   );
