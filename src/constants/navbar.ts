f144e76c4ab918581dec4d224425da7e10ee76cd
alpha0212 <qkrcjfgml321@naver.com>
2023-07-05 20:26:08 +0900

    feat(constants): Move to schedule day page according day

--- src/constants/navbar.ts
+++ src/constants/navbar.ts
@@ -1,7 +1,9 @@
 import { NavbarProps } from "src/components/common";
 
+const now = new Date();
+const day = now.getDate();
 export const NAVBAR_MENU: NavbarProps["menu"] = [
   { text: "한세톤 소개", href: "/" },
-  { text: "대회 일정", href: "/schedules" },
+  { text: "대회 일정", href: `/schedules/${day === 21 ? `Friday` : `Thursday`}` },
   { text: "후원사", href: "/sponsors" },
 ];
