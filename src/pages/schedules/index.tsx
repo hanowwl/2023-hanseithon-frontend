5f5dc7f0f8373b068173281981e48e5b0a469c40
alpha0212 <qkrcjfgml321@naver.com>
2023-07-07 01:53:35 +0900

    refactor(pages): implementing function using day.js

--- src/pages/schedules/index.tsx
+++ src/pages/schedules/index.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 import Link from "next/link";
 import { useRouter } from "next/router";
+
+import dayjs from "dayjs";
 
 import { PageLayout } from "src/components/layouts";
 import { ScheduleDateElement, ScheduleElementProps } from "src/components/schedule";
@@ -9,86 +11,62 @@ import { DAY_DATE } from "src/constants";
 
 import * as S from "./styled";
 
-// 현재 시간이 주어진 시간 범위에 포함되는지 확인하는 함수
-function isTimeInRange(timeRange: string, currentTime: string): boolean {
-  // 시간 문자열에서 시작 시간과 종료 시간 추출
-  const [startTime, endTime] = timeRange.split(" ~ ");
-
-  // 현재 시간을 시간과 분으로 분리
-  const [currentHour, currentMinute] = currentTime.split(":").map(Number);
-
-  // 시작 시간과 종료 시간을 시간과 분으로 분리
-  const [startHour, startMinute] = startTime.split(":").map(Number);
-  const [endHour, endMinute] = endTime.split(":").map(Number);
-
-  // 현재 시간과 시작 시간, 종료 시간 비교
-  if (currentHour > startHour && currentHour < endHour) {
-    return true;
-  } else if (currentHour === startHour && currentMinute >= startMinute) {
-    return true;
-  } else if (currentHour === endHour && currentMinute <= endMinute) {
-    return true;
-  }
-
-  return false;
-}
-
 export interface ScheduleTimeData {
   elementArray: ScheduleElementProps[];
 }
 
 export default function SchedulesPage() {
-  const now = new Date();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-  const day = now.getDate();
-  const [nowDate, setNowDate] = useState<string>(`${hours}:${minutes}`);
+  const NOW_CHECK_INTERVAL_SEC = 10;
 
   const router = useRouter();
-  const pageDateUrl = router.query.date;
+  const { date } = useMemo(() => router.query as { date: string }, [router]);
+
+  const [now, setNow] = useState(dayjs());
+  const activeDate = useMemo(() => {
+    if (now.isBetween("2023-07-20", "2023-07-21", "date", "[]")) return now.format("MM/DD");
+    return date || "07/20";
+  }, [now, date]);
 
   useEffect(() => {
-    setNowDate(`${hours}:${minutes}`);
-    router.replace(`/schedules?date=${day === 21 ? `7/21` : `7/20`}`);
-    //eslint-disable-next-line react-hooks/exhaustive-deps
+    router.replace(`/schedules?date=${activeDate}`);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [activeDate]);
+
+  useEffect(() => {
+    const interval = setInterval(() => setNow(dayjs()), 1000 * NOW_CHECK_INTERVAL_SEC);
+    return () => clearInterval(interval);
   }, []);
-
-  // 20일 이전인지 여부를 계산하는 변수
-  const isBefore20th = day <= 20;
-
-  // 20일 이전인 경우 해당 스케줄의 날짜를 저장
-  const targetDay = isBefore20th ? 20 : 21;
 
   return (
     <S.ScheduleSection>
       <PageLayout title="대회 일정" description="저희 한세톤은 아래와 같이 진행될 예정이에요!">
         <S.ScheduleContainer>
-          {DAY_DATE.map((date, idx) => {
-            // 현재 스케줄 날짜와 targetDay(20일 또는 21일)이 같은지 여부를 계산
-            const isCurrentDate = date.day === targetDay;
+          {DAY_DATE.map(({ date, schedules }, i) => {
+            const isToday = date.isToday() || date.format("MM/DD") === activeDate;
 
             return (
-              <S.ScheduleElementContainer current={pageDateUrl === date.dayName} key={idx}>
+              <S.ScheduleElementContainer key={i} current={isToday}>
                 <S.ScheduleUl>
                   <S.ScheduleLi>
                     <S.ScheduleUl>
-                      {/* Button 컴포넌트에 currentDateBool 전달 */}
                       <Link
                         style={{ textDecoration: "none" }}
-                        href={`/schedules?date=${date.dayName}`}
+                        href={`/schedules?date=${date.format("MM/DD")}`}
+                        replace
                       >
-                        <S.ShowScheduleButton isCurrentDate={pageDateUrl === date.dayName}>
-                          {date.detail}
+                        <S.ShowScheduleButton isCurrentDate={isToday}>
+                          {date.format("M월 DD일 (ddd)")}
                         </S.ShowScheduleButton>
                       </Link>
                     </S.ScheduleUl>
-                    {date.arr.map((data, idx) => (
+
+                    {schedules.map(({ startAt, endAt, schedule }, i) => (
                       <ScheduleDateElement
-                        key={idx}
-                        time={data.time}
-                        nowSchedule={data.schedule}
-                        isToday={isCurrentDate}
-                        isCurrentSchedule={day === date.day && isTimeInRange(data.time, nowDate)}
+                        key={i}
+                        time={`${startAt.format("HH:mm")} ~ ${endAt.format("HH:mm")}`}
+                        nowSchedule={schedule}
+                        isToday={isToday}
+                        isCurrentSchedule={now.isBetween(startAt, endAt, "minute", "[)")}
                       />
                     ))}
                   </S.ScheduleLi>
