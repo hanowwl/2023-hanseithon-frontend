7b3d943be093068a79d133cb8f5320638e6555bc
alpha0212 <qkrcjfgml321@naver.com>
2023-07-06 20:31:27 +0900

    refactor(constants): Change the type variable name to schedule

--- src/constants/scheduleData.ts
+++ src/constants/scheduleData.ts
@@ -1,67 +1,67 @@
 interface ScheduleData {
   time: string;
-  nowSchedule: string;
+  schedule: string;
 }
 
 export const THURSDAY_SCHEDULE_ARRAY: ScheduleData[] = [
   {
     time: "15:00 ~ 15:30",
-    nowSchedule: "참가자 입장 및 등록",
+    schedule: "참가자 입장 및 등록",
   },
   {
     time: "15:30 ~ 16:00",
-    nowSchedule: "키노트",
+    schedule: "키노트",
   },
   {
     time: "16:00 ~ 18:00",
-    nowSchedule: "자율 개발",
+    schedule: "자율 개발",
   },
   {
     time: "18:00 ~ 19:00",
-    nowSchedule: "저녁 식사/멘토단 오리엔테이션",
+    schedule: "저녁 식사/멘토단 오리엔테이션",
   },
   {
     time: "19:00 ~ 19:30",
-    nowSchedule: "이벤트 타임",
+    schedule: "이벤트 타임",
   },
   {
     time: "19:30 ~ 23:00",
-    nowSchedule: "자율 개발 및 멘토링",
+    schedule: "자율 개발 및 멘토링",
   },
   {
     time: "23:00 ~ 24:00",
-    nowSchedule: "야식 시간 및 자율 개발",
+    schedule: "야식 시간 및 자율 개발",
   },
 ];
 
 export const FRIDAY_SCHEDULE_ARRAY: ScheduleData[] = [
   {
     time: "08:00 ~ 09:00",
-    nowSchedule: "아침 식사 배부",
+    schedule: "아침 식사 배부",
   },
   {
     time: "09:00 ~ 12:00",
-    nowSchedule: "자율 개발 및 멘토링",
+    schedule: "자율 개발 및 멘토링",
   },
   {
     time: "12:00 ~ 14:00",
-    nowSchedule: "점심 식사 및 발표 준비",
+    schedule: "점심 식사 및 발표 준비",
   },
   {
     time: "14:00 ~ 15:30",
-    nowSchedule: "발표 및 심사",
+    schedule: "발표 및 심사",
   },
   {
     time: "15:30 ~ 16:00",
-    nowSchedule: "시상",
+    schedule: "시상",
   },
   {
     time: "15:00 ~ 18:00",
-    nowSchedule: "발표 및 심사",
+    schedule: "발표 및 심사",
   },
   {
     time: "18:00 ~ 18:30",
-    nowSchedule: "시상",
+    schedule: "시상",
   },
 ];
 
