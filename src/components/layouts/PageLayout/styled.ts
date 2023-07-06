1e2f7604c0a53a16bb29530446c3eefc96a91b07
HanSu Lee <i.hansu.lee@gmail.com>
2023-07-07 03:08:37 +0900

    refactor(components): add `gap` prop into `PageLayout` layout component

--- src/components/layouts/PageLayout/styled.ts
+++ src/components/layouts/PageLayout/styled.ts
@@ -1,15 +1,13 @@
 import styled from "@emotion/styled";
 
-export const PageLayoutInfoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  margin-top: 2rem;
+export const PageLayoutInfoContainer = styled.div<{ gap: string }>`
+  margin-bottom: ${(props) => props.gap};
 `;
 
 export const PageLayoutTitle = styled.h1`
   font-size: 2.4rem;
   font-weight: 700;
+  margin-bottom: 0.6rem;
 `;
 
 export const PageLayoutDescription = styled.p`
@@ -17,6 +15,5 @@ export const PageLayoutDescription = styled.p`
   font-size: 1.6rem;
   font-weight: 500;
 
-  margin-top: 0.6rem;
   letter-spacing: -0.064rem;
 `;
