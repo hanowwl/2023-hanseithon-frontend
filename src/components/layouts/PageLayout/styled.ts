8d49ab2e818ae99498afbd2c69e11923e547ad99
alpha0212 <qkrcjfgml321@naver.com>
2023-07-07 00:25:20 +0900

    refactor(components): add PageLayout style

--- src/components/layouts/PageLayout/styled.ts
+++ src/components/layouts/PageLayout/styled.ts
@@ -1,31 +1,22 @@
 import styled from "@emotion/styled";
 
-export const PageLayoutContainer = styled.div``;
-
-export const PageLayoutInfoContainer = styled.div``;
-
-export const PageLayoutTitle = styled.h1``;
-
-export const PageLayoutDescription = styled.p``;
-
-export const TitleContainer = styled.div`
+export const PageLayoutInfoContainer = styled.div`
   display: flex;
   flex-direction: column;
 
   margin-top: 2rem;
 `;
 
-export const TitleText = styled.h1`
+export const PageLayoutTitle = styled.h1`
   font-size: 2.4rem;
   font-weight: 700;
-
-  margin-bottom: 0.6rem;
 `;
 
-export const TitleDesc = styled.p`
+export const PageLayoutDescription = styled.p`
   color: #878b92;
   font-size: 1.6rem;
   font-weight: 500;
 
+  margin-top: 0.6rem;
   letter-spacing: -0.064rem;
 `;
