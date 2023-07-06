73765868d09d04b41dedc6d52c2dccd4396c720f
alpha0212 <qkrcjfgml321@naver.com>
2023-07-06 16:16:52 +0900

    refactor(constants): modify query string url

--- src/constants/navbar.ts
+++ src/constants/navbar.ts
@@ -4,6 +4,6 @@ const now = new Date();
 const day = now.getDate();
 export const NAVBAR_MENU: NavbarProps["menu"] = [
   { text: "한세톤 소개", href: "/" },
-  { text: "대회 일정", href: `/schedules/${day === 21 ? `Friday` : `Thursday`}` },
+  { text: "대회 일정", href: `/schedules?date=${day === 21 ? `7/21` : `7/20`}` },
   { text: "후원사", href: "/sponsors" },
 ];
