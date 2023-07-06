8e68fdd1890f99f49b5173ca78e0fbcf00095b30
alpha0212 <qkrcjfgml321@naver.com>
2023-07-06 20:30:03 +0900

    refactor(constants): remove date data

--- src/constants/navbar.ts
+++ src/constants/navbar.ts
@@ -1,9 +1,7 @@
 import { NavbarProps } from "src/components/common";
 
-const now = new Date();
-const day = now.getDate();
 export const NAVBAR_MENU: NavbarProps["menu"] = [
   { text: "한세톤 소개", href: "/" },
-  { text: "대회 일정", href: `/schedules?date=${day === 21 ? `7/21` : `7/20`}` },
+  { text: "대회 일정", href: `/schedules` },
   { text: "후원사", href: "/sponsors" },
 ];
