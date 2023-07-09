224fab724c92b152cd65e8b660e19e50d6605ea6
HanSu Lee <i.hansu.lee@gmail.com>
2023-07-09 22:05:49 +0900

    feat(components): add `TeamSection` component

---
+++ src/components/teams/TeamSection/styled.ts
@@ -0,0 +1,33 @@
+import styled from "@emotion/styled";
+
+export const TeamSection = styled.section`
+  width: 100%;
+`;
+
+export const TeamSectionHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 2.4rem;
+`;
+
+export const TeamSectionTitle = styled.h2`
+  font-size: 2rem;
+  font-weight: 700;
+  margin-bottom: 0.4rem;
+`;
+
+export const TeamSectionDescription = styled.p`
+  font-size: 1.4rem;
+  font-weight: 400;
+  color: ${(props) => props.theme.color.text.secondary.default};
+`;
+
+export const TeamSectionActionsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.4rem;
+`;
+
+export const TeamSectionChildrenContainer = styled.div``;
--- src/components/teams/index.ts
+++ src/components/teams/index.ts
@@ -1,2 +1,3 @@
 export * from "./NewUser";
 export * from "./NewTeam";
+export * from "./TeamSection";
