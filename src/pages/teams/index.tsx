1a725a71c4f5692daa846424fcce10db21b8af02
HanSu Lee <i.hansu.lee@gmail.com>
2023-07-10 02:32:58 +0900

    refactor(pages): rename styled components name in `Teams` page

--- src/pages/teams/index.tsx
+++ src/pages/teams/index.tsx
@@ -12,34 +12,42 @@ export default function TeamsPage() {
 
   return (
     <TeamLayout>
-      <TeamSection
-        title="새로 참여한 메이커"
-        description="한세톤에 참여한 메이커들을 환영해주세요!"
-      >
-        <S.NewMakerContainer>
-          {members.map(({ name, position, student, profileUrl, createdAt }, i) => (
-            <TeamMemberCard
-              key={i}
-              username={name}
-              position={position}
-              student={student}
-              profileUrl={profileUrl}
-              createdAt={createdAt}
-            />
-          ))}
-        </S.NewMakerContainer>
-      </TeamSection>
+      <S.TeamSectionsContainer>
+        <TeamSection
+          title="새로 참여한 메이커"
+          description="한세톤에 참여한 메이커들을 환영해주세요!"
+        >
+          <S.NewMakerListContainer>
+            {members.map(({ name, position, student, profileUrl, createdAt }, i) => (
+              <TeamMemberCard
+                key={i}
+                username={name}
+                position={position}
+                student={student}
+                profileUrl={profileUrl}
+                createdAt={createdAt}
+              />
+            ))}
+          </S.NewMakerListContainer>
+        </TeamSection>
 
-      <TeamSection
-        title="현재까지 참여한 팀"
-        description="현재까치 한세톤에 참여 신청한 전체 팀 리스트에요<"
-      >
-        <S.TeamsContainer>
-          {teams.map(({ type, name, description, members }, i) => (
-            <TeamCard key={i} type={type} name={name} description={description} members={members} />
-          ))}
-        </S.TeamsContainer>
-      </TeamSection>
+        <TeamSection
+          title="현재까지 참여한 팀"
+          description="현재까치 한세톤에 참여 신청한 전체 팀 리스트에요"
+        >
+          <S.TeamsGridContainer>
+            {teams.map(({ type, name, description, members }, i) => (
+              <TeamCard
+                key={i}
+                type={type}
+                name={name}
+                description={description}
+                members={members}
+              />
+            ))}
+          </S.TeamsGridContainer>
+        </TeamSection>
+      </S.TeamSectionsContainer>
     </TeamLayout>
   );
 }
--- src/pages/teams/styled.ts
+++ src/pages/teams/styled.ts
@@ -1,49 +1,24 @@
 import styled from "@emotion/styled";
 
-export const NewMakerContainer = styled.div`
+export const TeamSectionsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6.8rem;
+`;
+
+export const NewMakerListContainer = styled.div`
   width: 100%;
   display: flex;
   gap: 1.2rem;
-
   overflow-x: auto;
   ::-webkit-scrollbar {
     display: none;
   }
 `;
 
-export const TeamsTitle = styled.h1`
-  margin-top: 5.3rem;
-
-  font-size: 2rem;
-  font-weight: 700;
-  letter-spacing: -0.08rem;
-`;
-export const TeamsDescription = styled.p`
-  color: #878b92;
-
-  font-size: 1.4rem;
-  font-weight: 400;
-  letter-spacing: -0.056rem;
-
-  margin-top: 0.4rem;
-`;
-export const TeamsContainer = styled.div`
+export const TeamsGridContainer = styled.div`
+  width: 100%;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-`;
-
-//users
-export const UserInformationTagWrapper = styled.div`
-  margin-top: 0.4rem;
-  margin-right: 0.4rem;
-
-  border-radius: 0.5rem;
-  background-color: #1774fe;
-`;
-export const UserInformationTag = styled.p`
-  font-size: 1.2rem;
-  font-weight: 500;
-  letter-spacing: -0.048rem;
-
-  padding: 0.4rem 0.6rem;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1.2rem;
 `;
