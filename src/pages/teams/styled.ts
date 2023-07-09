09d2f66448b70cccd807778a8577bb3212e6cb38
alpha0212 <qkrcjfgml321@naver.com>
2023-07-09 23:48:52 +0900

    refactor(pages): change query to useQuery

--- src/pages/teams/styled.ts
+++ src/pages/teams/styled.ts
@@ -12,15 +12,6 @@ export const NewMakerContainer = styled.div`
   margin-top: 1.7rem;
 `;
 
-export const TeamsBanner = styled.div`
-  width: 100%;
-  height: 35.1rem;
-
-  margin-top: 2.5rem;
-  border-radius: 2.2rem;
-  background-color: #d9d9d9;
-`;
-
 export const TeamsTitle = styled.h1`
   margin-top: 5.3rem;
 
