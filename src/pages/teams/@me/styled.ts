8f201b43623792bd079326011aa7ad7f5a68430f
HanSu Lee <i.hansu.lee@gmail.com>
2023-07-10 01:53:15 +0900

    feat(pages): increase section row max height in `MyTeam` page

--- src/pages/teams/@me/styled.ts
+++ src/pages/teams/@me/styled.ts
@@ -4,7 +4,7 @@ export const TeamLogSectionsGridContainer = styled.div`
   width: 100%;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: minmax(32rem, 40rem);
+  grid-template-rows: minmax(32rem, 44rem);
   margin-top: 5rem;
 
   @media screen and (max-width: 991px) {
