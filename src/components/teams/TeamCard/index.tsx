7699216f04741596cd6d421ca2b30c8c6b615da1
HanSu Lee <i.hansu.lee@gmail.com>
2023-07-10 02:17:05 +0900

    refactor(components): rename styled components name, and restruct elements in `TeamCard` component

--- src/components/teams/TeamCard/index.tsx
+++ src/components/teams/TeamCard/index.tsx
@@ -1,73 +1,47 @@
-import React from "react";
+import React, { useMemo } from "react";
 
-import { TEAM_TYPE } from "src/constants/teams";
+import { TeamMember, TeamType } from "src/apis/services/teams/types";
 
 import * as S from "./styled";
 
 export interface TeamsProps {
-  type: string;
+  type: TeamType;
   name: string;
   description: string;
-  members: {
-    isLeader: boolean;
-    position: string;
-    name: string;
-    profileUrl: string;
-  }[];
+  members: TeamMember[];
 }
 
 export const TeamCard: React.FC<TeamsProps> = ({ type, name, description, members }) => {
-  //영문으로 되어있는 데이터를 한글로
-  const TranslateTEAM_TYPE = () => {
-    return `${TEAM_TYPE[type]}`;
-  };
-
-  //팀장 이름 렌더링
-  const renderMembers = () => {
-    return members.map((member, i) => {
-      if (member.isLeader) {
-        return (
-          <S.TeamLeader key={i}>
-            <S.TeamPosition>팀장</S.TeamPosition>
-            <S.TeamMember>{member.name}</S.TeamMember>
-          </S.TeamLeader>
-        );
-      } else {
-        return null;
-      }
-    });
-  };
-
-  //팀장이 아닌 외에 멤버들
-  const renderOtherMembers = () => {
-    return members
-      .filter((member) => !member.isLeader)
-      .map((member, i) => {
-        return (
-          <React.Fragment key={i}>
-            {i > 0 && " · "}
-            {member.name}
-          </React.Fragment>
-        );
-      });
-  };
+  const leader = useMemo(() => members.find((member) => member.isLeader), [members]);
+  const notLeader = useMemo(() => members.filter((member) => !member.isLeader), [members]);
 
   return (
-    <S.TeamElement>
-      <S.TeamInformation>
-        <S.TeamType>
-          <S.TeamTypeText>{TranslateTEAM_TYPE()}</S.TeamTypeText>
-        </S.TeamType>
-        <S.TeamName>{name}</S.TeamName>
-        <S.TeamDescription>{description}</S.TeamDescription>
-        <S.TeamPositionContainer>
-          {renderMembers()}
-          <S.TeamMembers>
-            <S.TeamPosition>팀원</S.TeamPosition>
-            <S.TeamMember>{renderOtherMembers()}</S.TeamMember>
-          </S.TeamMembers>
-        </S.TeamPositionContainer>
-      </S.TeamInformation>
-    </S.TeamElement>
+    <S.TeamCardContainer>
+      <S.TeamType>{TeamType[type]}</S.TeamType>
+
+      <S.TeamName>{name}</S.TeamName>
+      <S.TeamDescription>{description}</S.TeamDescription>
+
+      <S.TeamMembersContainer>
+        <S.TeamMemberListContainer>
+          <S.TeamMemberTypeLabel>팀장</S.TeamMemberTypeLabel>
+          <S.TeamMember>{leader?.name}</S.TeamMember>
+        </S.TeamMemberListContainer>
+
+        <S.TeamMemberListContainer>
+          <S.TeamMemberTypeLabel>팀원</S.TeamMemberTypeLabel>
+          {notLeader.map((member, i, arr) => {
+            const isLastMember = arr.length - 1 === i;
+
+            return (
+              <React.Fragment key={i}>
+                <S.TeamMember>{member.name}</S.TeamMember>
+                {!isLastMember && <span> · </span>}
+              </React.Fragment>
+            );
+          })}
+        </S.TeamMemberListContainer>
+      </S.TeamMembersContainer>
+    </S.TeamCardContainer>
   );
 };
--- src/components/teams/TeamCard/styled.ts
+++ src/components/teams/TeamCard/styled.ts
@@ -1,66 +1,61 @@
 import styled from "@emotion/styled";
 
-export const TeamElement = styled.div`
-  display: inline;
-  width: 37rem;
-  margin-top: 1.6rem;
-`;
-export const TeamInformation = styled.div`
+export const TeamCardContainer = styled.div`
+  min-width: 37rem;
   padding: 2rem 2.4rem;
   border-radius: 1.2rem;
   background-color: #171717;
 `;
-export const TeamType = styled.div`
-  width: 5.3rem;
-  height: 2.2rem;
 
-  border-radius: 0.5rem;
-  background-color: #1774fe;
-`;
-
-export const TeamTypeText = styled.p`
+export const TeamType = styled.p`
+  width: fit-content;
   padding: 0.4rem 0.6rem;
   font-size: 1.2rem;
   font-weight: 500;
   letter-spacing: -0.048rem;
+  border-radius: 0.5rem;
+  background-color: #1774fe;
+  margin-bottom: 0.8rem;
 `;
 
-export const TeamName = styled.h1`
+export const TeamName = styled.h3`
   font-size: 1.8rem;
   font-weight: 700;
   letter-spacing: -0.072rem;
-
-  margin-top: 0.6rem;
+  margin-bottom: 0.6rem;
 `;
+
 export const TeamDescription = styled.p`
   color: #878b92;
   font-size: 1.6rem;
   font-weight: 400;
   letter-spacing: -0.064rem;
-
-  margin-top: 0.4rem;
+  margin-bottom: 2rem;
 `;
 
-export const TeamPositionContainer = styled.div`
-  margin-top: 2rem;
-`;
-export const TeamLeader = styled.div`
+export const TeamMembersContainer = styled.div`
   display: flex;
+  flex-direction: column;
+  gap: 0.6rem;
 `;
-export const TeamMembers = styled.div`
+
+export const TeamMemberListContainer = styled.div`
   display: flex;
-  margin-top: 0.6rem;
+  align-items: center;
+
+  span {
+    margin: 0 0.4rem;
+  }
 `;
-export const TeamPosition = styled.h2`
-  color: #428eff;
+
+export const TeamMemberTypeLabel = styled.p`
   font-size: 1.6rem;
-  font-weight: 700;
-  letter-spacing: -0.064rem;
-
-  margin-right: 0.6rem;
+  font-weight: 600;
+  color: #428eff;
+  margin-right: 0.8rem;
 `;
+
 export const TeamMember = styled.p`
   font-size: 1.6rem;
   font-weight: 500;
-  letter-spacing: -0.064rem;
 `;
