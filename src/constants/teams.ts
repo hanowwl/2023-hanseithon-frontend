e859d881bd2c4328347e3b46dc33b8ff4f58057c
alpha0212 <qkrcjfgml321@naver.com>
2023-07-09 18:03:27 +0900

    feat(constants): add teams constant, data is user tags and team information

---
+++ src/constants/teams.ts
@@ -0,0 +1,24 @@
+export const TeamType: { [key: string]: string } = {
+  LIVING: "생활부문",
+  GAME: "게임부문",
+};
+
+export const gradeIcons: { [key: number]: string } = {
+  1: "🌱",
+  2: "🌿",
+  3: "🌳",
+};
+
+export const departmentTranslations: { [key: string]: string } = {
+  HACKING: "해킹보안과",
+  NETWORK: "네트워크보안과",
+  GAME: "게임과",
+  CLOUD: "클라우드보안과",
+  META: "메타버스게임과",
+};
+
+export const positionTranslations: { [key: string]: string } = {
+  PM: "기획자",
+  DEVELOPER: "개발자",
+  DESIGNER: "디자이너",
+};
