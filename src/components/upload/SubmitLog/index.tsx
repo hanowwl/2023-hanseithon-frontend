be9db9807ae418cc7eb15c97c18bef8081601f44
alpha0212 <qkrcjfgml321@naver.com>
2023-07-16 23:59:00 +0900

    refactor(components): add Button component

--- src/components/upload/SubmitLog/index.tsx
+++ src/components/upload/SubmitLog/index.tsx
@@ -6,19 +6,20 @@ import * as S from "./styled";
 
 export interface SubmitLogProps {
   children?: React.ReactNode;
-  fileCount: number;
-  ButtonNode: React.ReactNode;
+  uploadOnClick: React.ReactEventHandler;
 }
 
-export const SubmitLog: React.FC<SubmitLogProps> = ({ children, fileCount, ButtonNode }) => {
+export const SubmitLog: React.FC<SubmitLogProps> = ({ children, uploadOnClick }) => {
   return (
     <S.SubmitLogContainer>
       <S.SubmitTitleContainer>
-        <S.SubmitTitle>업로드 파일 (총 {fileCount}개)</S.SubmitTitle>
+        <S.SubmitTitle>업로드 파일</S.SubmitTitle>
         <S.SubmitDescription>등록된 업로드 예정 파일이에요</S.SubmitDescription>
       </S.SubmitTitleContainer>
       <S.FilesWrapper style={{ margin: "2.4rem 0", height: "31rem" }}>{children}</S.FilesWrapper>
-      {ButtonNode}
+      <Button fillWidth={true} onClick={uploadOnClick}>
+        파일 업로드
+      </Button>
     </S.SubmitLogContainer>
   );
 };
