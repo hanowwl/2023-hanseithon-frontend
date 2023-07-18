a2ecf417631e6678512cfa8a3281fd243bdcb3be
alpha0212 <qkrcjfgml321@naver.com>
2023-07-19 01:37:59 +0900

    refactor(components): UploadTrack change to forwardRef

--- src/components/upload/UploadTrack/index.tsx
+++ src/components/upload/UploadTrack/index.tsx
@@ -1,22 +1,21 @@
-import React from "react";
+import React, { InputHTMLAttributes } from "react";
 
 import * as S from "./styled";
 
-export interface UploadTrackProps {
-  onUploadFile: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  ref: React.MutableRefObject<HTMLInputElement | null>;
-}
+type UploadTrackProps = InputHTMLAttributes<HTMLInputElement>;
 
-export const UploadTrack: React.FC<UploadTrackProps> = ({ onUploadFile, ref }) => {
-  return (
-    <S.UploadTrack>
-      <label htmlFor="ex_file">
-        <S.UploadText>
-          파일을 이 곳에 드래그하거나, 클릭해 업로드할 파일을 선택해주세요 <br />
-          (zip 파일로 업로드해주세요.)
-        </S.UploadText>
-      </label>
-      <input ref={ref} id="ex_file" type="file" onChange={onUploadFile} />
-    </S.UploadTrack>
-  );
-};
+const UploadTrack = React.forwardRef<HTMLInputElement, UploadTrackProps>(({ ...options }, ref) => (
+  <S.UploadTrack>
+    <label htmlFor="ex_file">
+      <S.UploadText>
+        파일을 이 곳에 드래그하거나, 클릭해 업로드할 파일을 선택해주세요 <br />
+        (zip 파일로 업로드해주세요.)
+      </S.UploadText>
+    </label>
+    <input ref={ref} {...options} />
+  </S.UploadTrack>
+));
+
+UploadTrack.displayName = "Upload Track";
+
+export { UploadTrack };
