32b3340cb4618ae3ff1acf581b352473f4c31243
HanSu Lee <i.hansu.lee@gmail.com>
2023-07-20 08:58:15 +0900

    chore(constants): add schedule menu into team routes navbar menu

--- src/constants/navbar.ts
+++ src/constants/navbar.ts
@@ -3,12 +3,15 @@ import { NavbarProps } from "src/components/common";
 
 export const NAVBAR_MENU: NavbarProps["menu"] = [
   { text: "한세톤 소개", href: "/" },
-  { text: "대회 일정", href: `/schedules` },
+  { text: "대회 일정", href: "/schedules" },
   { text: "후원사", href: "/sponsors" },
 ];
 
 export const NAVBAR_MENU_IN_TEAM_ROUTES: (user?: User) => NavbarProps["menu"] = (user) => {
-  const menu = [{ text: "한세톤 소식", href: "/teams" }];
+  const menu = [
+    { text: "대회 일정", href: "/schedules" },
+    { text: "한세톤 소식", href: "/teams" },
+  ];
   if (user?.teamMember) menu.push({ text: "내 팀", href: "/teams/@me" });
 
   return menu;
