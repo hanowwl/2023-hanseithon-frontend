1afa0417dade25367cf376eeeb0312ada46ad02c
alpha0212 <qkrcjfgml321@naver.com>
2023-07-21 13:34:56 +0900

    feat(components): add hleft time to end into \TeamLayout\ component

--- src/components/layouts/TeamLayout/styled.ts
+++ src/components/layouts/TeamLayout/styled.ts
@@ -13,7 +13,34 @@ export const TeamLayoutBannerContainer = styled.div`
   position: relative;
   width: 100%;
   height: 36rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   margin-bottom: 5.2rem;
+  border-radius: 1.6rem;
+
+  background: conic-gradient(
+    from 180deg at 50% 50%,
+    #3f86cb 0deg,
+    #6da4da 71.25deg,
+    #cae5fb 172.5deg,
+    #82bff5 249.37deg,
+    #3f86cb 360deg
+  );
 `;
 
 export const TeamLayoutChildrenContainer = styled.main``;
+
+export const DeadLineText = styled.p`
+  font-size: 2.8rem;
+  margin-bottom: 1.2rem;
+`;
+
+export const DeadLineLeftTime = styled.p`
+  height: fit-content;
+  font-size: 9.2rem;
+  font-weight: 800;
+  letter-spacing: 0.01em;
+  text-align: center;
+`;
